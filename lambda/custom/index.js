12f86c1c96fe062b13d1cf6afec7108c2fc77084
kimuradev <leandro2ki@gmail.com>
2019-12-02 07:22:16 -0300

    Add start project

--- lambda/custom/index.js
+++ lambda/custom/index.js
@@ -1,21 +1,90 @@
-/* *
- * This sample demonstrates handling intents from an Alexa skill using the Alexa Skills Kit SDK (v2).
- * Please visit https://alexa.design/cookbook for additional examples on implementing slots, dialog management,
- * session persistence, api calls, and more.
- * */
+// This sample demonstrates handling intents from an Alexa skill using the Alexa Skills Kit SDK (v2).
+// Please visit https://alexa.design/cookbook for additional examples on implementing slots, dialog management,
+// session persistence, api calls, and more.
 const Alexa = require('ask-sdk-core');
-// i18n library dependency, we use it below in a localisation interceptor
-const i18n = require('i18next');
-// i18n strings for all supported locales
-const languageStrings = require('./languageStrings');
+const persistenceAdapter = require('ask-sdk-s3-persistence-adapter');
+const questions = require('./questions');
+
+const b200ms = ' <break time= "200ms" /> ';
+
+const messages = {
+    PERGUNTA_NOME : 'Olá, bem vindo ao Partiu Poupar! Me diga, qual é o seu nome?',
+    PERGUNTA_IDADE: 'E quantos anos você tem, {0}?',
+    COMEÇAR_PERGUNTAS: '{0}, preciso te fazer algumas perguntas, pra te conhecer melhor.' + b200ms + 'Para começarmos, diga a palavra iniciar ou começar.',
+    
+    USER: 'Seu nome é {0}'
+    
+}
+
+let SCORE = 0;
+
+function calculaScore(idade) {
+    console.log('IDADE: ' + idade)
+    SCORE += scoreIdade(idade)
+    console.log('SCORE: '+ SCORE);        
+}
+
+function scoreIdade(idade) {
+    if (idade <= 25) {
+        return 3;
+    } else if (idade >= 26 && idade <= 40) {
+        return 2;
+    } else if (idade >= 41 && idade <= 64) {
+        return 1;
+    } else {
+        return 0;
+    }
+}
+
+function formatCasing(key) {
+  return key.split(/(?=[A-Z])/).join(' ');
+}
+
+function getQuestion(counter, property, item) {
+  return `Here is your ${counter}th question.  What is the ${formatCasing(property)} of ${item.StateName}?`;
+}
+
+function getRandom(min, max) {
+  return Math.floor((Math.random() * ((max - min) + 1)) + min);
+}
+
+function askQuestion(handlerInput) {
+  console.log("I am in askQuestion()");
+  //GENERATING THE RANDOM QUESTION FROM DATA
+  const random = getRandom(0, questions.length - 1);
+  const item = questions[random];
+  const propertyArray = Object.getOwnPropertyNames(item);
+  const property = propertyArray[getRandom(1, propertyArray.length - 1)];
+
+  //GET SESSION ATTRIBUTES
+  const attributes = handlerInput.attributesManager.getSessionAttributes();
+
+  //SET QUESTION DATA TO ATTRIBUTES
+  attributes.selectedItemIndex = random;
+  attributes.quizItem = item;
+  attributes.quizProperty = property;
+  attributes.counter += 1;
+
+  //SAVE ATTRIBUTES
+  handlerInput.attributesManager.setSessionAttributes(attributes);
+
+  const question = getQuestion(attributes.counter, property, item);
+  return question;
+}
+
+String.prototype.format = function() {
+	var args = arguments;
+	return this.replace(/\{(\d+)\}/g, function(text, key) {
+		return args[key];
+	});
+}
 
 const LaunchRequestHandler = {
     canHandle(handlerInput) {
         return Alexa.getRequestType(handlerInput.requestEnvelope) === 'LaunchRequest';
     },
     handle(handlerInput) {
-        const speakOutput = handlerInput.t('WELCOME_MSG');
-
+        const speakOutput = messages.PERGUNTA_NOME;
         return handlerInput.responseBuilder
             .speak(speakOutput)
             .reprompt(speakOutput)
@@ -23,19 +92,80 @@ const LaunchRequestHandler = {
     }
 };
 
-const HelloWorldIntentHandler = {
+const UserIntentHandler = {
     canHandle(handlerInput) {
         return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
-            && Alexa.getIntentName(handlerInput.requestEnvelope) === 'HelloWorldIntent';
+            && Alexa.getIntentName(handlerInput.requestEnvelope) === 'UserIntent';
     },
-    handle(handlerInput) {
-        const speakOutput = handlerInput.t('HELLO_MSG');
-
+    async handle(handlerInput) {
+        const nome = handlerInput.requestEnvelope.request.intent.slots.nome.value;
+        const idade = handlerInput.requestEnvelope.request.intent.slots.idade.value;
+        
+        calculaScore(idade);
+        
+        const attributesManager = handlerInput.attributesManager;
+        
+         const userAttributes = {
+            "nome" : nome,
+            "idade" : idade,
+        };
+        attributesManager.setSessionAttributes(userAttributes);
+        
+        await attributesManager.savePersistentAttributes();
+        
+        const sessionAttributes = handlerInput.attributesManager.getSessionAttributes();
+        
+        const speakOutput = messages.COMEÇAR_PERGUNTAS.format(nome);
         return handlerInput.responseBuilder
             .speak(speakOutput)
-            //.reprompt('add a reprompt if you want to keep the session open for the user to respond')
+            //.reprompt(speakOutput)
             .getResponse();
     }
+};
+
+
+const QuestionsIntentHandler = {
+    canHandle(handlerInput) {
+        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
+            && Alexa.getIntentName(handlerInput.requestEnvelope) === 'QuestionsIntent';
+    },
+    handle(handlerInput) {
+        console.log("Inside QuizHandler - handle");
+        const sessionAttributes = handlerInput.attributesManager.getSessionAttributes();
+        const response = handlerInput.responseBuilder;
+        //sessionAttributes.state = states.QUIZ;
+        sessionAttributes.counter = 0;
+        sessionAttributes.quizScore = 0;
+
+        var question = askQuestion(handlerInput);
+        var speakOutput = 'Primeira pergunta: ' + question;
+        var repromptOutput = question;
+    
+        //const nome = sessionAttributes.nome;
+        //const speakOutput = messages.USER.format(nome);
+        return handlerInput.responseBuilder
+            .speak(speakOutput)
+            .reprompt(speakOutput)
+            .getResponse();
+    }
+};
+
+const RepeatHandler = {
+  canHandle(handlerInput) {
+    console.log("Inside RepeatHandler");
+    return handlerInput.requestEnvelope.request.type === 'IntentRequest'
+        && handlerInput.requestEnvelope.request.intent.name === 'AMAZON.RepeatIntent';
+  },
+  handle(handlerInput) {
+    console.log("Inside RepeatHandler - handle");
+    const attributes = handlerInput.attributesManager.getSessionAttributes();
+    const question = getQuestion(attributes.counter, attributes.quizproperty, attributes.quizitem);
+
+    return handlerInput.responseBuilder
+      .speak(question)
+      .reprompt(question)
+      .getResponse();
+  },
 };
 
 const HelpIntentHandler = {
@@ -44,7 +174,7 @@ const HelpIntentHandler = {
             && Alexa.getIntentName(handlerInput.requestEnvelope) === 'AMAZON.HelpIntent';
     },
     handle(handlerInput) {
-        const speakOutput = handlerInput.t('HELP_MSG');
+        const speakOutput = 'Vou te fazer algumas perguntas, pra conseguir identificar qual é o seu perfil de investidor. Basta responder o número de cada pergunta, para que no final, eu te diga qual é o seu perfil.';
 
         return handlerInput.responseBuilder
             .speak(speakOutput)
@@ -52,7 +182,6 @@ const HelpIntentHandler = {
             .getResponse();
     }
 };
-
 const CancelAndStopIntentHandler = {
     canHandle(handlerInput) {
         return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
@@ -60,59 +189,34 @@ const CancelAndStopIntentHandler = {
                 || Alexa.getIntentName(handlerInput.requestEnvelope) === 'AMAZON.StopIntent');
     },
     handle(handlerInput) {
-        const speakOutput = handlerInput.t('GOODBYE_MSG');
-
+        const speakOutput = 'Tchau!';
         return handlerInput.responseBuilder
             .speak(speakOutput)
             .getResponse();
     }
 };
-/* *
- * FallbackIntent triggers when a customer says something that doesn’t map to any intents in your skill
- * It must also be defined in the language model (if the locale supports it)
- * This handler can be safely added but will be ingnored in locales that do not support it yet 
- * */
-const FallbackIntentHandler = {
-    canHandle(handlerInput) {
-        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
-            && Alexa.getIntentName(handlerInput.requestEnvelope) === 'AMAZON.FallbackIntent';
-    },
-    handle(handlerInput) {
-        const speakOutput = handlerInput.t('FALLBACK_MSG');
 
-        return handlerInput.responseBuilder
-            .speak(speakOutput)
-            .reprompt(speakOutput)
-            .getResponse();
-    }
-};
-/* *
- * SessionEndedRequest notifies that a session was ended. This handler will be triggered when a currently open 
- * session is closed for one of the following reasons: 1) The user says "exit" or "quit". 2) The user does not 
- * respond or says something that does not match an intent defined in your voice model. 3) An error occurs 
- * */
 const SessionEndedRequestHandler = {
     canHandle(handlerInput) {
         return Alexa.getRequestType(handlerInput.requestEnvelope) === 'SessionEndedRequest';
     },
     handle(handlerInput) {
-        console.log(`~~~~ Session ended: ${JSON.stringify(handlerInput.requestEnvelope)}`);
         // Any cleanup logic goes here.
-        return handlerInput.responseBuilder.getResponse(); // notice we send an empty response
+        return handlerInput.responseBuilder.getResponse();
     }
 };
-/* *
- * The intent reflector is used for interaction model testing and debugging.
- * It will simply repeat the intent the user said. You can create custom handlers for your intents 
- * by defining them above, then also adding them to the request handler chain below 
- * */
+
+// The intent reflector is used for interaction model testing and debugging.
+// It will simply repeat the intent the user said. You can create custom handlers
+// for your intents by defining them above, then also adding them to the request
+// handler chain below.
 const IntentReflectorHandler = {
     canHandle(handlerInput) {
         return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest';
     },
     handle(handlerInput) {
         const intentName = Alexa.getIntentName(handlerInput.requestEnvelope);
-        const speakOutput = handlerInput.t('REFLECTOR_MSG', {intentName: intentName});
+        const speakOutput = `You just triggered ${intentName}`;
 
         return handlerInput.responseBuilder
             .speak(speakOutput)
@@ -120,18 +224,17 @@ const IntentReflectorHandler = {
             .getResponse();
     }
 };
-/**
- * Generic error handling to capture any syntax or routing errors. If you receive an error
- * stating the request handler chain is not found, you have not implemented a handler for
- * the intent being invoked or included it in the skill builder below 
- * */
+
+// Generic error handling to capture any syntax or routing errors. If you receive an error
+// stating the request handler chain is not found, you have not implemented a handler for
+// the intent being invoked or included it in the skill builder below.
 const ErrorHandler = {
     canHandle() {
         return true;
     },
     handle(handlerInput, error) {
-        const speakOutput = handlerInput.t('ERROR_MSG');
-        console.log(`~~~~ Error handled: ${JSON.stringify(error)}`);
+        console.log(`~~~~ Error handled: ${error.stack}`);
+        const speakOutput = `Desculpe, tive um problema em entender o que voce disse. Por favor, tente denovo.`;
 
         return handlerInput.responseBuilder
             .speak(speakOutput)
@@ -140,34 +243,27 @@ const ErrorHandler = {
     }
 };
 
-// This request interceptor will bind a translation function 't' to the handlerInput
-const LocalisationRequestInterceptor = {
-    process(handlerInput) {
-        i18n.init({
-            lng: Alexa.getLocale(handlerInput.requestEnvelope),
-            resources: languageStrings
-        }).then((t) => {
-            handlerInput.t = (...args) => t(...args);
-        });
-    }
-};
-/**
- * This handler acts as the entry point for your skill, routing all request and response
- * payloads to the handlers above. Make sure any new handlers or interceptors you've
- * defined are included below. The order matters - they're processed top to bottom 
- * */
+
+
+
+// The SkillBuilder acts as the entry point for your skill, routing all request and response
+// payloads to the handlers above. Make sure any new handlers or interceptors you've
+// defined are included below. The order matters - they're processed top to bottom.
 exports.handler = Alexa.SkillBuilders.custom()
+    .withPersistenceAdapter(
+        new persistenceAdapter.S3PersistenceAdapter({bucketName:process.env.S3_PERSISTENCE_BUCKET})
+    )
     .addRequestHandlers(
         LaunchRequestHandler,
-        HelloWorldIntentHandler,
+        UserIntentHandler,
+        QuestionsIntentHandler,
+        RepeatHandler,
         HelpIntentHandler,
         CancelAndStopIntentHandler,
-        FallbackIntentHandler,
         SessionEndedRequestHandler,
-        IntentReflectorHandler)
+        IntentReflectorHandler, // make sure IntentReflectorHandler is last so it doesn't override your custom intent handlers
+    )
     .addErrorHandlers(
-        ErrorHandler)
-    .addRequestInterceptors(
-        LocalisationRequestInterceptor)
-    .withCustomUserAgent('sample/hello-world/v1.2')
+        ErrorHandler,
+    )
     .lambda();
